1611078d2db5e49f8b75418a4bf7dc42935fb572
FaustXVI <xavier.detant@gmail.com>
2018-07-28 11:04:39 +0200

    Clean up

--- infrastructure/manual/create-pipeline.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# create S3 bucket
-aws codepipeline create-pipeline --pipeline file://simple-pipeline.json
--- infrastructure/manual/create-watch.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-aws events put-rule --name watch-git --event-pattern file://watch-git-event.json 
-aws events put-targets --rule watch-git --targets file://trigger-pipeline.json
