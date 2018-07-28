201f7924ad5bcfd7f85d13c0038af003eea495e3
FaustXVI <xavier.detant@gmail.com>
2018-07-28 10:04:35 +0200

    Remove useless dependencies

--- default.nix
+++ default.nix
@@ -1,25 +1,8 @@
 with import <nixpkgs> {};
-let
-  my-glibc = glibc;
-  my-gcc = gcc-unwrapped.lib;
-  my-gmp = gmp;
-in stdenv.mkDerivation {
-    name = "elm-sandbox";
+stdenv.mkDerivation {
+    name = "aws-demo";
     buildInputs = [
         jq
         awscli
-        nodejs
    ];
-    patchPhase = ''
-        patchShebangs node_modules/webpack
-        '';
-    shellHook = ''
-        export PATH="$PATH:$(pwd)/node_modules/.bin"
-        patchelf \
-            --debug \
-            --set-interpreter ${my-glibc}/lib/ld-linux-x86-64.so.2 \
-            --set-rpath "${builtins.concatStringsSep ":" (map (l: "${l}/lib") [my-gcc my-gmp my-glibc])}" \
-            node_modules/elm-test/bin/elm-interface-to-json
-        '';
-
 }
